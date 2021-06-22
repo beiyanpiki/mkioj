ed46192679ec125260e273185bd41613a3aca182
beiyanpiki <sizegaoxin@126.com>
2021-06-23 00:51:50 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		Hello, world! You had click button below {{ cnt }} times.
+		<button @click="onAddBtnClick">+1</button>
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent, ref } from 'vue'
+
+	export default defineComponent({
+		setup() {
+			const cnt = ref(1)
+			const onAddBtnClick = () => {
+				cnt.value++
+			}
+			return { cnt, onAddBtnClick }
+		},
+	})
+</script>
