dbcbbf7f3ad13b5b424c6c531ee7a0fc76b6188b
beiyanpiki <sizegaoxin@126.com>
2021-06-24 00:19:25 +0800

    feat(countdown): add countdown component

---
+++ src/component/CountDown/CountDown.vue
@@ -0,0 +1,35 @@
+<template>
+	<span>{{ formatSecondToHMS(remain) }}</span>
+</template>
+
+<script lang="ts">
+	import { onMounted, defineComponent, ref } from 'vue'
+	import { formatSecondToHMS } from '@/util/time'
+
+	export default defineComponent({
+		props: {
+			start: { type: Number, required: true },
+			end: { type: Number, required: true },
+		},
+		setup({ start, end }) {
+			let ticker: NodeJS.Timer | null = null
+			const remain = ref(end - start)
+
+			const startCountDown = () => {
+				ticker = setInterval(() => {
+					console.log(remain.value)
+					remain.value -= 1
+					if (remain <= 0) {
+						remain.value = 0
+						clearInterval(ticker)
+					}
+				}, 1000)
+			}
+
+			onMounted(() => {
+				startCountDown()
+			})
+			return { formatSecondToHMS, remain }
+		},
+	})
+</script>
